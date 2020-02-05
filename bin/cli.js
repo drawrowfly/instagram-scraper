bcd750d6728224e8f8036775354088dd28f973ba
drawrowfly <anonim.megamim@gmail.com>
2020-02-05 13:14:50 +0100

    scrape following/followers

--- bin/cli.js
+++ bin/cli.js
@@ -1,127 +1,101 @@
 #!/usr/bin/env node
-"use strict"; 
- 
-const instagramScrape = require("../lib/instance");
+'use strict';
 
-const startScraper = async (argv) => {
-    try{
+const instagramScrape = require('../lib/instance');
+
+const startScraper = async argv => {
+    try {
         argv.scrapeType = argv._[0];
         argv.cli = true;
+
         let scraper = await instagramScrape(argv).getPosts();
 
-        if (scraper.zip){
+        if (scraper.zip) {
             console.log(`ZIP path: ${scraper.zip}`);
         }
-        if (scraper.json){
+        if (scraper.json) {
             console.log(`JSON path: ${scraper.json}`);
         }
-        if (scraper.csv){
+        if (scraper.csv) {
             console.log(`CSV path: ${scraper.csv}`);
         }
-
-    }catch(error){
+    } catch (error) {
         console.log(error.message);
     }
-}
+};
 
-require("yargs")
+require('yargs')
     .usage('Usage: $0 <command> [options]')
-    .command(
-        "user [id]", 
-        "Scrape posts from username. Enter only username", 
-        {}, 
-        (argv) => {
-            startScraper(argv);
-        }
-    )
-    .command(
-        "hashtag [id]", 
-        "Scrape posts from hashtag. Enter hashtag without #", 
-        {}, 
-        (argv) => {
-            startScraper(argv);
-        }
-    )
-    .command(
-        "location [id]", 
-        "Scrape posts from a specific location. Enter location ID", 
-        {}, 
-        (argv) => {
-            startScraper(argv);
-        }
-    )
-    .command(
-        "comments [id]", 
-        "Scrape comments from a post. Enter post url or post id", 
-        {}, 
-        (argv) => {
-            startScraper(argv);
-        }
-    )
-    .command(
-        "likers [id]", 
-        "Scrape users who liked a post. Enter post url or post id", 
-        {}, 
-        (argv) => {
-            startScraper(argv);
-        }
-    )
+    .command('user [id]', 'Scrape posts from username. Enter only username', {}, argv => {
+        startScraper(argv);
+    })
+    .command('hashtag [id]', 'Scrape posts from hashtag. Enter hashtag without #', {}, argv => {
+        startScraper(argv);
+    })
+    .command('location [id]', 'Scrape posts from a specific location. Enter location ID', {}, argv => {
+        startScraper(argv);
+    })
+    .command('comments [id]', 'Scrape comments from a post. Enter post url or post id', {}, argv => {
+        startScraper(argv);
+    })
+    .command('likers [id]', 'Scrape users who liked a post. Enter post url or post id', {}, argv => {
+        startScraper(argv);
+    })
     .options({
-        "help": {
-            alias: "h",
-            describe: "help"
+        help: {
+            alias: 'h',
+            describe: 'help',
         },
-        "count": {
-            alias: "c",
+        count: {
+            alias: 'c',
             default: 0,
-            describe: "Number of post to scrape"
+            describe: 'Number of post to scrape',
         },
-        "mediaType": {
-            alias: "m",
-            default: "all",
-            choices: ["image", "video", "all"],
-            describe: "Media type to scrape"
+        mediaType: {
+            alias: 'm',
+            default: 'all',
+            choices: ['image', 'video', 'all'],
+            describe: 'Media type to scrape',
         },
-        "proxy": {
-            alias: "p",
-            default: "",
-            describe: "Set proxy"
+        proxy: {
+            alias: 'p',
+            default: '',
+            describe: 'Set proxy',
         },
-        "timeout": {
+        timeout: {
             default: 0,
-            describe: "If you will receive error saying 'rate limit', you can try to set timeout. Timeout is in mls: 1000 mls = 1 second"
+            describe: "If you will receive error saying 'rate limit', you can try to set timeout. Timeout is in mls: 1000 mls = 1 second",
         },
-        "download": {
-            alias: "d",
+        download: {
+            alias: 'd',
             boolean: true,
             default: false,
-            describe: "Download and archive all scraped posts to a ZIP file"
+            describe: 'Download and archive all scraped posts to a ZIP file',
         },
-        "asyncDownload": {
-            alias: "a",
+        asyncDownload: {
+            alias: 'a',
             default: 5,
-            describe: "How many posts should be downloaded at the same time. Try not to set more then 5 "
+            describe: 'How many posts should be downloaded at the same time. Try not to set more then 5 ',
         },
-        "progress": {
+        progress: {
             boolean: true,
             default: true,
-            describe: "Show progress in terminal"
+            describe: 'Show progress in terminal',
         },
-        "filename":{
-            alias: ["file", "f"],
-            default: "[id]",
-            describe: "Name of the output file",
+        filename: {
+            alias: ['file', 'f'],
+            default: '[id]',
+            describe: 'Name of the output file',
         },
-        "filepath":{
-            default: process.cwd(), //${os.homedir()}
-            describe: "Directory to save all output files.",
+        filepath: {
+            default: process.cwd(),
+            describe: 'Directory to save all output files.',
         },
-        "filetype": {
-            alias: ["type", "t"],
-            default: "csv",
-            choices: ["csv", "json", "all"],
+        filetype: {
+            alias: ['type', 't'],
+            default: 'csv',
+            choices: ['csv', 'json', 'all'],
             describe: "Type of output file where post information will be saved. 'all' - save information about all posts to a 'json' and 'csv' ",
         },
     })
-    .demandCommand()
-    .argv
+    .demandCommand().argv;
--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-"use strict";
+'use strict';
 
-const os = require("os");
+const os = require('os');
 
-const instaTouch = require("./lib/instance");
+const instaTouch = require('./lib/instance');
 
 let INIT_OPTIONS = {
     id: '',
@@ -17,166 +17,193 @@ let INIT_OPTIONS = {
     progress: false,
 };
 
-const startScraper = ( options ) => {
-    return new Promise( async (resolve, reject) => {
-        if (!options.filepath){
+const startScraper = options => {
+    return new Promise(async (resolve, reject) => {
+        if (!options.filepath) {
             options.filepath = INIT_OPTIONS.filepath;
         }
 
-        if (!options.filetype){
+        if (!options.filetype) {
             options.filetype = INIT_OPTIONS.filetype;
         }
 
-        if (!options.mediaType){
+        if (!options.mediaType) {
             options.mediaType = INIT_OPTIONS.mediaType;
         }
 
-        if (!options.filename){
+        if (!options.filename) {
             options.filename = INIT_OPTIONS.filename;
-        } 
+        }
 
-        if (!options.asyncDownload){
+        if (!options.asyncDownload) {
             options.asyncDownload = INIT_OPTIONS.asyncDownload;
         }
 
-        try{
+        try {
             return resolve(await instaTouch(options).getPosts());
-        } catch(error){
+        } catch (error) {
             return reject(error);
         }
+    });
+};
 
-    })
-}
-
-exports.hashtag = ( id, options ) => {
-    if (typeof(options) !== "object" ){
-        throw new Error("Object is expected");
+exports.hashtag = (id, options) => {
+    if (typeof options !== 'object') {
+        throw new Error('Object is expected');
     }
-    options = Object.assign(INIT_OPTIONS, options)
+    options = Object.assign(INIT_OPTIONS, options);
 
-    options.scrapeType = "hashtag";
+    options.scrapeType = 'hashtag';
     options.id = id;
 
-    if (options.event){
+    if (options.event) {
         return instaTouch(options);
-    }else{
-        return new Promise( async (resolve, reject) => {
-            try{
+    } else {
+        return new Promise(async (resolve, reject) => {
+            try {
                 return resolve(await startScraper(options));
-            }catch(error){
+            } catch (error) {
                 return reject(error);
             }
-
-        })
+        });
     }
-}
+};
 
-exports.location = ( id, options ) => {
-    if (typeof(options) !== "object" ){
-        throw new Error("Object is expected");
+exports.location = (id, options) => {
+    if (typeof options !== 'object') {
+        throw new Error('Object is expected');
     }
-    options = Object.assign(INIT_OPTIONS, options)
+    options = Object.assign(INIT_OPTIONS, options);
 
-    options.scrapeType = "location";
+    options.scrapeType = 'location';
     options.id = id;
 
-    if (options.event){
+    if (options.event) {
         return instaTouch(options);
-    }else{
-        return new Promise( async(resolve, reject) => {
-            try{
+    } else {
+        return new Promise(async (resolve, reject) => {
+            try {
                 return resolve(await startScraper(options));
-            }catch(error){
+            } catch (error) {
                 return reject(error);
             }
-        })
+        });
     }
-}
+};
 
-exports.user = ( id, options ) => {
-    if (typeof(options) !== "object" ){
-        throw new Error("Object is expected");
+exports.user = (id, options) => {
+    if (typeof options !== 'object') {
+        throw new Error('Object is expected');
     }
-    options = Object.assign(INIT_OPTIONS, options)
+    options = Object.assign(INIT_OPTIONS, options);
 
-    options.scrapeType = "user";
+    options.scrapeType = 'user';
     options.id = id;
 
-    if (options.event){
+    if (options.event) {
         return instaTouch(options);
-    }else{
-        return new Promise( async (resolve, reject) => {
-                try{
-                    return resolve(await startScraper(options));
-                }catch(error){
-                    return reject(error);
-                }
-        })
+    } else {
+        return new Promise(async (resolve, reject) => {
+            try {
+                return resolve(await startScraper(options));
+            } catch (error) {
+                return reject(error);
+            }
+        });
     }
-}
+};
 
-exports.comments = ( id, options ) => {
-    if (typeof(options) !== "object" ){
-        throw new Error("Object is expected");
+exports.comments = (id, options) => {
+    if (typeof options !== 'object') {
+        throw new Error('Object is expected');
     }
-    options = Object.assign(INIT_OPTIONS, options)
+    options = Object.assign(INIT_OPTIONS, options);
 
-    options.scrapeType = "comments";
+    options.scrapeType = 'comments';
     options.id = id;
 
-    if (options.event){
+    if (options.event) {
         return instaTouch(options);
-    }else{
-        return new Promise( async (resolve, reject) => {
-                try{
-                    return resolve(await startScraper(options));
-                }catch(error){
-                    return reject(error);
-                }
-        })
+    } else {
+        return new Promise(async (resolve, reject) => {
+            try {
+                return resolve(await startScraper(options));
+            } catch (error) {
+                return reject(error);
+            }
+        });
     }
-}
+};
 
-exports.likers = ( id, options ) => {
-    if (typeof(options) !== "object" ){
-        throw new Error("Object is expected");
+exports.likers = (id, options) => {
+    if (typeof options !== 'object') {
+        throw new Error('Object is expected');
     }
-    options = Object.assign(INIT_OPTIONS, options)
+    options = Object.assign(INIT_OPTIONS, options);
 
-    options.scrapeType = "likers";
+    options.scrapeType = 'likers';
     options.id = id;
 
-    if (options.event){
+    if (options.event) {
         return instaTouch(options);
-    }else{
-        return new Promise( async (resolve, reject) => {
-                try{
-                    return resolve(await startScraper(options));
-                }catch(error){
-                    return reject(error);
-                }
-        })
+    } else {
+        return new Promise(async (resolve, reject) => {
+            try {
+                return resolve(await startScraper(options));
+            } catch (error) {
+                return reject(error);
+            }
+        });
     }
-}
+};
 
-exports.followers = ( id, options ) => {
-    if (typeof(options) !== "object" ){
-        throw new Error("Object is expected");
+exports.followers = (id, options) => {
+    if (typeof options !== 'object') {
+        throw new Error('Object is expected');
     }
-    options = Object.assign(INIT_OPTIONS, options)
 
-    options.scrapeType = "followers";
+    if (!options.sessionId) {
+        throw new Error('Selected type requires an active session ID');
+    }
+    options = Object.assign(INIT_OPTIONS, options);
+
+    options.scrapeType = 'followers';
     options.id = id;
 
-    if (options.event){
+    if (options.event) {
         return instaTouch(options);
-    }else{
-        return new Promise( async (resolve, reject) => {
-                try{
-                    return resolve(await startScraper(options));
-                }catch(error){
-                    return reject(error);
-                }
-        })
+    } else {
+        return new Promise(async (resolve, reject) => {
+            try {
+                return resolve(await startScraper(options));
+            } catch (error) {
+                return reject(error);
+            }
+        });
     }
-}
+};
+
+exports.following = (id, options) => {
+    if (typeof options !== 'object') {
+        throw new Error('Object is expected');
+    }
+    if (!options.sessionId) {
+        throw new Error('Selected type requires an active session ID');
+    }
+    options = Object.assign(INIT_OPTIONS, options);
+
+    options.scrapeType = 'following';
+    options.id = id;
+
+    if (options.event) {
+        return instaTouch(options);
+    } else {
+        return new Promise(async (resolve, reject) => {
+            try {
+                return resolve(await startScraper(options));
+            } catch (error) {
+                return reject(error);
+            }
+        });
+    }
+};
